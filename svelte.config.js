7433d501845de407e12a4cf95a103375a14e36b8
Artem Nikitin <a.nikitin@dataduck.com>
2024-08-18 00:29:48 +0200

    fix deploy (hope so)

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,11 @@ import preprocess from "svelte-preprocess";
 export default {
 	kit: {
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			fallback: undefined,
-			precompress: false,
-			strict: true,
+			fallback: "404.html",
 		}),
+		paths: {
+			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+		},
 	},
 	preprocess: preprocess(),
 };
